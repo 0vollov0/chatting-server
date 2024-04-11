9ed6f017711312fda8e5023d035a5c6b1daffac4
SinGyu Park <psk876@naver.com>
2024-04-12 00:17:08 +0900

    remove useless

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from 'apps/common/src/common.module';
@@ -8,7 +6,5 @@ import { ChatRoomsModule } from './chat-rooms/chat-rooms.module';
 
 @Module({
   imports: [CommonModule, UsersModule, AuthModule, ChatRoomsModule],
-  controllers: [ApiController],
-  providers: [ApiService],
 })
 export class ApiModule {}

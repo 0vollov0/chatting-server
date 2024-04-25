1ef862f9780c47f9c03886d27c53e3222515cc70
SinGyu Park <psk876@naver.com>
2024-04-25 23:35:38 +0900

    remove useless

--- apps/common/src/common.module.ts
+++ apps/common/src/common.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
 import { DatabasesModule } from './databases/databases.module';
 import { RedisModule } from './redis/redis.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [ConfigModule, DatabasesModule, RedisModule],
-  providers: [CommonService],
 })
 export class CommonModule {}
